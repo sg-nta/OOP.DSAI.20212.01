c7727eb0e22e88b89db2a3d5570afd322c397b03
babushkatty <truongan1412@gmail.com>
2022-05-21 12:00:32 +0700

    lab04

--- AimsProject/src/DigitalVideoDisc.java
+++ AimsProject/src/DigitalVideoDisc.java
@@ -1,4 +1,75 @@
 
-public class DigitalVideoDisc {
 
+public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+	private int id;
+	private static int nbDigitalVideoDisc = 0;
+	
+	
+	public static int getNbDigitalVideoDisc() {
+		return nbDigitalVideoDisc;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+		nbDigitalVideoDisc++;
+		this.id = nbDigitalVideoDisc;
+	}
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+		nbDigitalVideoDisc++;
+		this.id = nbDigitalVideoDisc;
+	}
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+		nbDigitalVideoDisc++;
+		this.id = nbDigitalVideoDisc;
+	}
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+		nbDigitalVideoDisc++;
+		this.id = nbDigitalVideoDisc;
+		
+	}
+	
+	
+	
 }
+
---
+++ AimsProject/src/TestPassingParameter.java
@@ -0,0 +1,41 @@
+
+public class TestPassingParameter {
+	public static void main(String[] args) {
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+		newkindaObject jungledvd = new newkindaObject(jungleDVD);
+		newkindaObject cinderelladvd = new newkindaObject(cinderellaDVD);
+		
+		swap(jungledvd, cinderelladvd);
+		System.out.println("jungle title: " + jungledvd.diffObject.getTitle());
+		System.out.println("cinderella title: " + cinderelladvd.diffObject.getTitle());
+		
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("jungle title: " + jungleDVD.getTitle());
+		
+				
+	}
+	
+    public static void swap(newkindaObject o1, newkindaObject o2) {
+    	DigitalVideoDisc tmp;
+    	tmp = o1.diffObject;
+    	o1.diffObject = o2.diffObject;
+    	o2.diffObject = tmp;
+    }
+    
+    static class newkindaObject {
+    	DigitalVideoDisc diffObject;
+    	public newkindaObject(DigitalVideoDisc newobj) {
+    		this.diffObject = newobj;
+    	}
+    }
+    
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+    	String oldTitle = dvd.getTitle();
+    	dvd.setTitle(title);
+    	dvd = new DigitalVideoDisc(oldTitle);
+    	
+    }
+}
+
+
