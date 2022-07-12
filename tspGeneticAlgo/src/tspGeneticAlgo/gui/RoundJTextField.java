e12d6d1bee73a46a0f0ddc27a18a48f5cde949dd
hoang van an <96238764+EthAnHoangg@users.noreply.github.com>
2022-07-13 06:16:47 +0700

    update diagram and fix GUI

--- tspGeneticAlgo/src/tspGeneticAlgo/gui/RoundJTextField.java
+++ tspGeneticAlgo/src/tspGeneticAlgo/gui/RoundJTextField.java
@@ -2,26 +2,18 @@ package tspGeneticAlgo.gui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.RoundRectangle2D;
 
 public class RoundJTextField extends JTextField {
-    private Shape shape;
     public RoundJTextField(String text, int columns) {
         super(text, columns);
     }
-    protected void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
         g.setColor(getBackground());
-        g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+        g.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
         super.paintComponent(g);
     }
-    protected void paintBorder(Graphics g) {
+    public void paintBorder(Graphics g) {
         g.setColor(Color.WHITE);
-        g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
-    }
-    public boolean contains(int x, int y) {
-        if (shape == null || !shape.getBounds().equals(getBounds())) {
-            shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
-        }
-        return shape.contains(x, y);
+        g.drawRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
     }
 }
